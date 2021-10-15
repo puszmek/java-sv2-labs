957ab7481bcbce59f479c33b9283cad9240cf9f1
he <bgyhenrietta@yahoo.com>
2021-10-16 00:36:15 +0200

    Registration.java added

---
+++ training-solutions/src/main/java/classstructureio/Registration.java
@@ -0,0 +1,23 @@
+package classstructureio;
+
+import java.util.Scanner;
+
+public class Registration {
+
+    public static void main(String[] args) {
+
+        System.out.println("Create New Account");
+
+        System.out.println("Username:");
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine();
+
+        System.out.println("E-mail address:");
+        String email = scanner.nextLine();
+
+        System.out.println("Account Details:");
+        System.out.println("Username: " + userName);
+        System.out.println("E-mail address: "+ email);
+
+    }
+}
