471340b61e93ef47893cff403278b3046952cfd4
he <bgyhenrietta@yahoo.com>
2021-10-16 00:59:29 +0200

    Calculator.java added

---
+++ training-solutions/src/main/java/classstructureio/Calculator.java
@@ -0,0 +1,23 @@
+package classstructureio;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter two integers!");
+
+        System.out.println("First integer:");
+        Scanner scanner = new Scanner(System.in);
+        int first = scanner.nextInt();
+
+        System.out.println("Second integer:");
+        int second = scanner.nextInt();
+
+        System.out.println("Sum of two integers:");
+        System.out.println(first + " + " + second + " = ");
+        System.out.println(first + second);
+
+    }
+}
