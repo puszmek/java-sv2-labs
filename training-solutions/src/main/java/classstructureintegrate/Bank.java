fa603ee44bf7ceacafe2ff5eba1f6424ae8a052f
he <bgyhenrietta@yahoo.com>
2021-10-17 22:55:45 +0200

    Gyakorlati feladat - Bankszámlák

---
+++ training-solutions/src/main/java/classstructureintegrate/Bank.java
@@ -0,0 +1,35 @@
+package classstructureintegrate;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Bank {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your account number?");
+        String accountNumber = scanner.nextLine();
+
+        System.out.println("What is your name?");
+        String owner = scanner.nextLine();
+
+        System.out.println("Your start balance?");
+        int balance = scanner.nextInt();
+
+        BankAccount bankAccount = new BankAccount(accountNumber, owner, balance);
+        System.out.println(bankAccount.getInfo());
+
+        System.out.println("Deposit: ");
+        int deposit = scanner.nextInt();
+        bankAccount.deposit(deposit);
+        System.out.println("New balance: " + bankAccount.getInfo());
+
+        System.out.println("Withdraw: ");
+        int withdraw = scanner.nextInt();
+        bankAccount.withdraw(withdraw);
+        System.out.println("New balance: " + bankAccount.getInfo());
+
+    }
+
+}
