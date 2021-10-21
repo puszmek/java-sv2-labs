82f06ebdfbfb9eb2ab6bc59d39f94fa930a1b0c2
he <bgyhenrietta@yahoo.com>
2021-10-22 01:10:26 +0200

    Gyakorlati feladat - String osztály

---
+++ training-solutions/src/main/java/javaapi/Upper.java
@@ -0,0 +1,9 @@
+package javaapi;
+
+public class Upper {
+
+    public static void main(String[] args) {
+        String text = "Hello World!";
+        System.out.println(text.toUpperCase());
+    }
+}
