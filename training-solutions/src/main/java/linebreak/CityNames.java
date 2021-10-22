487f35f1953958daec1f64be544455058731e4ed
he <bgyhenrietta@yahoo.com>
2021-10-23 01:53:54 +0200

    Gyakorlati feladat - Településnevek

---
+++ training-solutions/src/main/java/linebreak/CityNames.java
@@ -0,0 +1,9 @@
+package linebreak;
+
+public class CityNames {
+
+    public static void main(String[] args) {
+
+        System.out.println("Budapest" + System.lineSeparator() + "London" + System.lineSeparator() + "NewYork");
+    }
+}
