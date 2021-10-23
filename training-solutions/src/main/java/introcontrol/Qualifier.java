930ef7868205c96faa95e84c522a9f232a657ebd
he <bgyhenrietta@yahoo.com>
2021-10-23 22:20:43 +0200

    Gyakorlati feladat - Minősítő

---
+++ training-solutions/src/main/java/introcontrol/Qualifier.java
@@ -0,0 +1,19 @@
+package introcontrol;
+
+import java.util.Scanner;
+
+public class Qualifier {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Adj meg egy számot!");
+        int number = scanner.nextInt();
+
+        if (number > 100) {
+            System.out.println("Nagyobb, mint száz.");
+        } else {
+            System.out.println("Száz, vagy kisebb.");
+        }
+    }
+}
