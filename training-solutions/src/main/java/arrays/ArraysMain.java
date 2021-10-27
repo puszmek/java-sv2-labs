63bd7e3bab2eae21c6fd9e7fa653a0861a040e76
he <bgyhenrietta@yahoo.com>
2021-10-27 02:52:20 +0200

    Gyakorlati feladat - Hónapok napjainak száma, Szorzótábla, Hőmérsékleti értékek, Hőmérsékleti értékek és az óraátállítás, Lottószámok

---
+++ training-solutions/src/main/java/arrays/ArraysMain.java
@@ -0,0 +1,68 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class ArraysMain {
+
+    public String numberOfDaysAsString() {
+        int[] numberOfDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        return Arrays.toString(numberOfDays);
+    }
+
+    public String multiplicationTableAsString(int size) {
+        int[][] multiplicationTable = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                multiplicationTable[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return Arrays.deepToString(multiplicationTable);
+    }
+
+    public boolean sameTempValues(double[] day, double[] anotherDay) {
+        return Arrays.equals(day, anotherDay);
+    }
+
+    public boolean sameTempValuesDaylight(double[] day, double[] anotherDay) {
+        int min = min(day.length, anotherDay.length);
+        double[] rangeOfDay = Arrays.copyOfRange(day, 0, min);
+        double[] rangeOfAnotherDay = Arrays.copyOfRange(anotherDay, 0, min);
+        return Arrays.equals(rangeOfDay, rangeOfAnotherDay);
+    }
+
+    private int min(int a, int b) {
+        if (a < b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public boolean wonLottery(int[] winner, int[] stake) {
+        int[] copyOfWinner = Arrays.copyOf(winner, winner.length);
+        int[] copyOfStake = Arrays.copyOf(stake, stake.length);
+        Arrays.sort(copyOfWinner);
+        Arrays.sort(copyOfStake);
+        return Arrays.equals(copyOfWinner, copyOfStake);
+    }
+
+    public static void main(String[] args) {
+
+        ArraysMain arraysMain = new ArraysMain();
+
+        System.out.println(arraysMain.numberOfDaysAsString());
+
+        System.out.println(arraysMain.multiplicationTableAsString(10));
+
+        System.out.println(arraysMain.sameTempValues(new double[]{2, 2, 2}, new double[]{2, 1, 2}));
+
+        System.out.println(arraysMain.sameTempValuesDaylight(new double[]{1, 2}, new double[]{1, 2, 3}));
+
+        int[] winner = new int[]{2, 3, 5, 7};
+        int[] stake = new int[]{7, 5, 3, 2};
+        System.out.println(arraysMain.wonLottery(winner, stake));
+        System.out.println(Arrays.toString(winner));
+        System.out.println(Arrays.toString(stake));
+        System.out.println(arraysMain.wonLottery(new int[]{1, 5, 9}, new int[]{1, 5, 7}));
+    }
+}
