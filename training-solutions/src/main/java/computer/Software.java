d7334444fefb21cb7924858ed3fe8ba13d689c7e
he <bgyhenrietta@yahoo.com>
2021-11-03 18:58:16 +0100

    Gyakorlati feladat - Számítógép

---
+++ training-solutions/src/main/java/computer/Software.java
@@ -0,0 +1,34 @@
+package computer;
+
+public class Software {
+
+    private String name;
+    private double numberOfVersion;
+
+    public Software(String name, double numberOfVersion) {
+        this.name = name;
+        this.numberOfVersion = numberOfVersion;
+    }
+
+    @Override
+    public String toString() {
+        return "Software: name: " + name +
+                ", numberOfVersion: " + numberOfVersion;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getNumberOfVersion() {
+        return numberOfVersion;
+    }
+
+    public void setNumberOfVersion(double numberOfVersion) {
+        this.numberOfVersion = numberOfVersion;
+    }
+}
