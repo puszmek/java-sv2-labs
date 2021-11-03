b85e2c774affa1ebd64b954802ab88890835a0d3
he <bgyhenrietta@yahoo.com>
2021-11-03 21:04:14 +0100

    Gyakorlati feladat - Számítógép

--- training-solutions/src/main/java/compositionlist/computer/PersonalComputer.java
+++ training-solutions/src/main/java/compositionlist/computer/PersonalComputer.java
@@ -1,4 +1,4 @@
-package computer;
+package compositionlist.computer;
 
 import java.util.ArrayList;
 import java.util.List;
--- training-solutions/src/main/java/compositionlist/computer/PersonalComputerMain.java
+++ training-solutions/src/main/java/compositionlist/computer/PersonalComputerMain.java
@@ -1,4 +1,4 @@
-package computer;
+package compositionlist.computer;
 
 public class PersonalComputerMain {
 
