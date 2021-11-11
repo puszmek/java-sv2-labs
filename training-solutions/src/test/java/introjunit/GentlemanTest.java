4a5983e484c3bcdded7de3614ff3348dba609287
he <bgyhenrietta@yahoo.com>
2021-11-11 22:12:50 +0100

    Gyakorlati feladat - Első teszt implementálása, Hibás teszt, Tesztlefedettség mérése

--- training-solutions/src/test/java/introjunit/GentlemanTest.java
+++ training-solutions/src/test/java/introjunit/GentlemanTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
-public class GenlemanTest {
+public class GentlemanTest {
 
     @Test
     public void testSayHello() {
