ed00a2a1116ded7713a32b8082bbe9623957efb8
he <bgyhenrietta@yahoo.com>
2021-11-11 17:19:21 +0100

    Gyakorlati feladat - Hibás teszt

--- training-solutions/src/test/java/introjunit/GenlemanTest.java
+++ training-solutions/src/test/java/introjunit/GenlemanTest.java
@@ -13,4 +13,21 @@ public class GenlemanTest {
         String text = gentleman.sayHello("John Doe");
         assertThat(text, equalTo("Hello John Doe"));
     }
+
+/* Gyakorlati feladat - Hibás teszt
+
+    Hibás program:
+    java.lang.AssertionError:
+    Expected: "Hello John Doe"
+         but: was "Hell John Doe"
+    Expected :Hello John Doe
+    Actual   :Hell John Doe
+
+    Hibás teszt:
+    java.lang.AssertionError:
+    Expected: "Hi John Doe"
+         but: was "Hello John Doe"
+    Expected :Hi John Doe
+    Actual   :Hello John Doe
+ */
 }
