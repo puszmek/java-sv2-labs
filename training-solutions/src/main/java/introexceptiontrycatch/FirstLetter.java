f09d348de6aa6c9e40750711fa2ecc3b643cfdbc
he <bgyhenrietta@yahoo.com>
2021-11-17 22:01:10 +0100

    Gyakorlati feladat - Első betű

--- training-solutions/src/main/java/introexceptiontrycatch/FirstLetter.java
+++ training-solutions/src/main/java/introexceptiontrycatch/FirstLetter.java
@@ -7,14 +7,15 @@ public class FirstLetter {
 
     public static void main(String[] args) {
 
-        List<String> words = Arrays.asList("egy", "kettő", "három", null);
-
-        try {
-            for (String word : words) {
+        List<String> words = Arrays.asList("egy", "kettő", "három", "", null);
+        for (String word: words) {
+            try {
                 System.out.println(word.charAt(0));
+            } catch (StringIndexOutOfBoundsException stringIndexOutOfBoundsException) {
+                System.out.println(stringIndexOutOfBoundsException.getMessage());
+            } catch (NullPointerException npe) {
+                System.out.println(npe.getMessage());
             }
-        } catch (NullPointerException npe) {
-            System.out.println(npe.getMessage());
         }
     }
 }
