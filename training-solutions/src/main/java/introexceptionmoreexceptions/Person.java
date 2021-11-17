e60be27417557c9f209216d97d77e3fe0771362d
he <bgyhenrietta@yahoo.com>
2021-11-17 12:36:56 +0100

    Gyakorlati feladat - Egy névtelen ember

---
+++ training-solutions/src/main/java/introexceptionmoreexceptions/Person.java
@@ -0,0 +1,34 @@
+package introexceptionmoreexceptions;
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "name: " + name + ", age: " + age;
+    }
+
+    public static void main(String[] args) {
+
+        Person person = new Person(null, 32);
+        System.out.println(person);
+        System.out.println(person.getName());
+        System.out.println(person.getName().toUpperCase());
+        // Az utolsó utasításnál keletkezik kivétel (NullPointerException), mert null értékű Stringen akarjuk meghívni a toUpperCase() metódust.
+    }
+}
