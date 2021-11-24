a0c534faae1db36d607f7a172d85112de4d05d62
he <bgyhenrietta@yahoo.com>
2021-11-24 06:33:14 +0100

    Gyakorlati feladat - Lövegcső

--- training-solutions/src/main/java/introexceptionthrowjunit4/Tank.java
+++ training-solutions/src/main/java/introexceptionthrowjunit4/Tank.java
@@ -5,11 +5,10 @@ public class Tank {
     private int angle;
 
     public void modifyAngle(int angleNumber) {
-        angleNumber += angle;
-        if (angleNumber < -80 || angleNumber > 80) {
+        angle += angleNumber;
+        if (angle < -80 || angle > 80) {
             throw new IllegalArgumentException("A tank lövegcsöve jobbra és balra is csak 80°-ot tud fordulni! A megadott értéknek -80 és 80 közötti számnak kell lennie!");
         }
-        angle = angleNumber;
     }
 
     public int getAngle() {
