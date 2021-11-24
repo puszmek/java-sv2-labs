460caf14d413fda7390dcb7aa9d0e784939cd7bf
he <bgyhenrietta@yahoo.com>
2021-11-24 07:22:49 +0100

    Gyakorlati feladat - Nyaralási lista

---
+++ training-solutions/src/main/java/introexceptionreadfile/Holiday.java
@@ -0,0 +1,19 @@
+package introexceptionreadfile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Holiday {
+
+    public static void main(String[] args) {
+
+        try {
+            List<String> holiday = Files.readAllLines(Paths.get("src/main/resources/holiday.txt"));
+            System.out.println(holiday);
+        } catch (IOException ioe) {
+            System.out.println("A fájl megnyitása sikertelen!");
+        }
+    }
+}
