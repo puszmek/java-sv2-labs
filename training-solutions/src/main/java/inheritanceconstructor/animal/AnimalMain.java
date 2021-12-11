eac393a98d56c19ec75fcfd88226ccad692ef8b7
he <bgyhenrietta@yahoo.com>
2021-12-11 05:18:10 +0100

    Gyakorlati feladat - Konstruktorok hívási lánca

---
+++ training-solutions/src/main/java/inheritanceconstructor/animal/AnimalMain.java
@@ -0,0 +1,15 @@
+package inheritanceconstructor.animal;
+
+public class AnimalMain {
+
+    public static void main(String[] args) {
+
+        Sparrow sparrow = new Sparrow();
+
+        /*
+        Animal class constructor
+        Bird class constructor
+        Sparrow class constructor
+        */
+    }
+}
