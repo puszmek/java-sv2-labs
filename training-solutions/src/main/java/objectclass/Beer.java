60a3589400619f9aa55bcfca554b050510c115be
he <bgyhenrietta@yahoo.com>
2021-12-11 12:09:44 +0100

    Gyakorlati feladat - A táska tartalma

---
+++ training-solutions/src/main/java/objectclass/Beer.java
@@ -0,0 +1,12 @@
+package objectclass;
+
+public class Beer {
+
+    private String name;
+    private int price;
+
+    public Beer(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
---
+++ training-solutions/src/main/java/objectclass/Book.java
@@ -0,0 +1,12 @@
+package objectclass;
+
+public class Book {
+
+    private String author;
+    private String title;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+}
