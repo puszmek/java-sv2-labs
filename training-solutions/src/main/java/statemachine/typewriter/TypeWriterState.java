47b33999acdf512bd5f44733df059713b3eaacbc
he <bgyhenrietta@yahoo.com>
2022-01-12 09:54:19 +0100

    Gyakorlati feladat - Elromlott az írógép

---
+++ training-solutions/src/main/java/statemachine/typewriter/TypeWriterState.java
@@ -0,0 +1,17 @@
+package statemachine.typewriter;
+
+public enum TypeWriterState {
+
+    LOWERCASE{
+        TypeWriterState next() {
+            return TypeWriterState.UPPERCASE;
+        }
+    },
+    UPPERCASE{
+        TypeWriterState next() {
+            return TypeWriterState.LOWERCASE;
+        }
+    };
+
+    abstract TypeWriterState next();
+}
